0a6bfb02042cd12df19f8999316637348ba97556
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-19 21:37:19 +0100

    import and export checked now babel work fine ..

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import  isBabelWork, { bel7aG } from './myModule'
 console.log(`wwassup GraphQL`)
+let a = ["Javascript", "is", (() => "Developer")()]
+console.log(isBabelWork(["Belhassen", ...a]))
---
+++ src/myModule.js
@@ -0,0 +1,5 @@
+const bel7aG = `Belhassen Gharsallah`
+
+const isBabelWork = (...a) => a
+
+export {  bel7aG, isBabelWork as default }
