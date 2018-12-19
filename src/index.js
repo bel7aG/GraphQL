cbe38ff0b77c877e46bfe115e97f28b1b6f82610
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-19 22:32:19 +0100

    create my first GraphQL Playground using graphql-yoga library so cool :D

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-import  isBabelWork, { bel7aG } from './myModule'
-console.log(`wwassup GraphQL`)
-let a = ["Javascript", "is", (() => "Developer")()]
-console.log(isBabelWork(["Belhassen", ...a]))
+import { GraphQLServer } from 'graphql-yoga'
+
+                        /* step 1: Type Defenition */
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`
+// ! for in hello means that hello always get a string
+                        /* step 2: Resolvers */
+const resolvers = {
+  Query: {
+    hello() {
+      return 'this is my first Query im so excited'
+    }
+  }
+}
+
+                          /*step 3: Create GraphQL Server  */
+
+const myServer = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+myServer.start(() => {
+  console.log('the server is Running!')
+})
--- src/myModule.js
+++
@@ -1,5 +0,0 @@
-const bel7aG = `Belhassen Gharsallah`
-
-const isBabelWork = (...a) => a
-
-export {  bel7aG, isBabelWork as default }
